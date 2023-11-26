374e64e69913aaf145d6a68a7381784cd0168b89
Minte-grace <mintesnottsegaye23@gmail.com>
2023-11-25 20:54:25 -0500

    MA-remove-find-usage

--- LinearSearch.js
+++ LinearSearch.js
@@ -1,7 +1,11 @@
-function linearSearch(n, t){
-    let target = n.find((item)=> item === t)
-    return n.indexOf(target) ?? -1
+function linearSearch(arr, t) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == t) {
+      return i;
+    }
+  }
+  return -1;
 }
 
-console.log(linearSearch([1,3, 5, 6, 7, 8] , 0));
-console.log(linearSearch([4, 6,2,1,3], 3));
+console.log(linearSearch([1, 3, 5, 6, 7, 8], 0));
+console.log(linearSearch([4, 6, 2, 1, 3], 3));
