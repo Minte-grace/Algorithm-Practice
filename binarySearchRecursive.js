31f3b29974a67a8ca797a7bdf274e50e3ad2ab03
Minte-grace <mintesnottsegaye23@gmail.com>
2023-11-25 21:47:47 -0500

    MA-binary-search

--- binarySearchRecursive.js
+++ binarySearchRecursive.js
@@ -1,12 +1,21 @@
-function binarySearchRecursive(arr, target, start, end){
-    let mid  = Math.floor((start + end)/ 2);
-
+function binarySearchHelper(arr, target, start, end){
     if(start > end) return -1;
+
+    let mid  = Math.floor((start + end) / 2);
 
     if(arr[mid] == target) return mid;
 
+    if(arr[mid] < target){
+        return binarySearchHelper(arr, target, mid + 1, end);
+    } else {
+        return binarySearchHelper(arr , target, start, mid - 1)
+    }
 }
 
+
+function binarySearchRecursive(arr, target) {
+    return binarySearchHelper(arr, target, 0, arr.length - 1);
+}
 
 console.log(binarySearchRecursive([-5, 2, 4, 6, 10], 10));
 console.log(binarySearchRecursive([-5, 2, 4, 6, 10], 6));
